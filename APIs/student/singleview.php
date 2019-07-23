c8e23e3748ccbdcbe2a0f574ee6542329435e062
jibanbarman <48866661+jibanbarman@users.noreply.github.com>
2019-07-23 20:40:58 +0530

    REST apis

---
+++ APIs/student/singleview.php
@@ -0,0 +1,25 @@
+
+<?php 
+   header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  include_once '../../config/Database.php';
+  include_once '../../model/student.php';
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+  // Instantiate object
+  $student = new Student($db);
+ 
+  // Get ID
+  $student->id = isset($_GET['id']) ? $_GET['id'] : die();
+  // Get student
+  $student->read_single();
+  // Create array
+  $arr = array(
+    'id' => $student->id,
+    's_name' => $student->s_name,
+    'age' => $student->age,
+    'field' => $student->field
+  );
+  print_r(json_encode($arr));
+  ?>
---
+++ APIs/teacher/create.php
@@ -0,0 +1,28 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+  include_once '../../config/Database.php';
+  include_once '../../model/teacher.php';
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+  // Instantiate  object
+  $teacher = new Teacher($db);
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+  $teacher->t_name = $data->t_name;
+  $teacher->subject = $data->subject;
+  // Create entry
+  if($teacher->create()) {
+    echo json_encode(
+      array('message' => 'Teacher Data Entry Created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Teacher Data Entry Not Created')
+    );
+  }
+  ?>
